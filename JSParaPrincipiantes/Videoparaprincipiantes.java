bfc81ee04068f4959f2c5f33b8e837ea7df0ec72
Jovana417 <jovana.cruzcruz20@gmail.com>
2025-09-10 18:18:23 -0600

    La creación de JavaScript

---
+++ JSParaPrincipiantes/Videoparaprincipiantes.java
@@ -0,0 +1,5 @@
+//Historia de la creación del lenguaje de programación JavaScript...
+//En el año 1995 se creo JavaScript por la pelea de dos empresas una era Netscape y Microsoft cada una tenía un navegador,
+//las querían que fueran las más utilizadas por los usuarios es decir querían que solo una fuera mejor y estaban agregando funcinalidad a sus navegadores
+//de manera que los programadores puedan crear sistios web de manera más atractiva para el usuario,
+//y de esta manera Netscape creo JavaScript era que los desarrolladores pudieran hacer que los sitios o páginas web pudieran hacer mucho más es decir que pudieran hacer mini videojuegos, animaciones entre otros .
